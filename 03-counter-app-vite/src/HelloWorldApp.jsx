bde75102c70b3601d9a19ed3f702afe3d8f23606
Alan Alvarez <alvarezalanfranco@gmail.com>
2024-10-14 17:01:09 -0300

    Creacion de primer componente simple.

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,14 @@
+/*
+function App() {
+    return(<h1>Hola Mundo!!!</h1>);
+}
+*/
+
+import React from 'react'
+
+export const HelloWorldApp = () => {
+  return (
+    <div>HelloWorldApp</div>
+  )
+}
+export default HelloWorldApp
