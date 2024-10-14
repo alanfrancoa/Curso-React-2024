c3869c0e9a501129e841ab05197d8fe2ad6f7767
Alan Alvarez <alvarezalanfranco@gmail.com>
2024-10-14 17:34:18 -0300

    Se realizo clase de impresion de variables en html.

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
+
+const getResult = (a, b) => a+b;
+
+
 const FirstApp = () => {
-  return (
-    <>//Fragment, para que funcione nuestro componente
-      <h1>First App</h1>
+
+  return ( //Fragment, para que funcione nuestro componente
+    <>
+
+      <h1>{getResult(666, 1)}</h1>
+      {/*<code>{ JSON.stringify(newMessage)}</code> */}
       <p>Soy un subtitulo</p>
     </>
+
+    //Podemos utilizar un JSON.stringify(objeto), si queremos mostrar todo el objeto.
   )
 }
 
