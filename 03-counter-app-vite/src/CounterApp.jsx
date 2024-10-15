39a70856944eb9023ff254d1809f144f6f781d18
Alan Alvarez <alvarezalanfranco@gmail.com>
2024-10-15 17:21:32 -0300

    Se realiza tarea de clase UseState

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -17,19 +17,24 @@ const CounterApp = ({value}) => {
 
     const [counter, setCounter]= useState(value)
 
+    const handleSubstract = () => setCounter( counter - 1 )
+
     const handleAdd = () => {
         setCounter( counter + 1 )
         //setCounter( (c) => c + 1 )
     }
+
+    const handleReset = () => setCounter(value)
     
   return (
     <>
     <h1>CounterApp</h1>
     <h2> {counter} </h2>
 
-    <button onClick={handleAdd}> {/*(e) => handleAdd(e) //en realidad seria asi, pero lo obviamos por ser el mismo parametro*/}
-        +1
-    </button>
+    <button onClick={handleAdd}> +1 </button>{/*(e) => handleAdd(e) //en realidad seria asi, pero lo obviamos por ser el mismo parametro*/}
+    <button onClick={handleSubstract}> -1 </button>
+    <button onClick={handleReset}> Reset </button>
+    
     </>
   )
 }
