d32fc5d9d29e069eea948d8ac774a1c80af1335f
Alan Alvarez <alvarezalanfranco@gmail.com>
2024-10-28 13:49:49 -0300

    Se realiza clase de testing con Jest: Expected, toBe y Describe.

--- 03-counter-app-vite/tests/demo.test.js
+++ 03-counter-app-vite/tests/demo.test.js
@@ -1,8 +1,18 @@
 
+describe('Pruebas en <DemoComponent />', () => { 
+    //con describe, se pueden agrupar distintas pruebas.
 
-
-test('Esta prueba no deberia fallar', () => {
-    if (1 === 0){
-        throw new Error('No se puede dividir por Cero');
-    }
-})
+    test('Esta prueba no deberia fallar', () => {
+        //1. Inicializacion 
+        const message1 = 'Hola Mundo';
+        // al hacer que tenga los espacios, no son iguales.
+        //2. Estimulo
+        const message2 = message1.trim();
+    
+        //3. Aserciones
+        
+        expect(message1).toBe(message2);
+        //En Jest, podemos evaluar igualdades de esta manera. 
+    
+    })
+ })
