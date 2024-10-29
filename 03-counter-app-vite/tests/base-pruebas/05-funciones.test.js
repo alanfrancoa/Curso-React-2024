a1a12152f19f90cbbf908db5c7a6a02cace09fb4
Alan Alvarez <alvarezalanfranco@gmail.com>
2024-10-29 17:17:20 -0300

    Se realiza test, en archivo 05-funciones, tambien se aplica toEqual y toStrictEqual.

---
+++ 03-counter-app-vite/tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,31 @@
+import { getUser, getUsuarioActivo } from "../../src/base-pruebas/05-funciones"
+
+describe('Pruebas en 05-funciones', () => {
+    test('getUser debe retornar un objeto', () => {
+
+        const testUser = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        }
+
+        const user = getUser();
+        console.log(user)
+
+        //Cuando tenemos dos objetos, al tener distinta ubicacion de memoria, NO SON IGUALES. Debemos usar toEqual o toEstrictEqual
+        expect( testUser ).toEqual( user );
+    })
+
+    test('getUsuarioActivo, debe retornar un objeto', () => { 
+
+        const name = 'Alan';
+
+        const testUser = {
+            uid: 'ABC567',
+            username: name
+        }
+
+        const user = getUsuarioActivo(name);
+
+        expect( testUser ).toEqual( user );
+    })
+})
